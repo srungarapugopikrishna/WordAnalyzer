ec672b84cdfc4cf8bafda4ec4f620ff631db47ac
srungarapugopikrishna <srungarapugopikrishna@gmail.com>
2016-05-08 12:33:21 +0530

    1. Displaying the word frequency based on the starting alphabet 2. Analyzing a given word

---
+++ README.md
@@ -0,0 +1,2 @@
+## English Word Analyzer
+TODO: Add readme about this application
---
+++ static/js/code.js
@@ -0,0 +1,74 @@
+
+
+
+  function myFunction(){
+  text = document.getElementById("box").value;
+  var myUrl = "http://localhost:5000/abcd";
+  console.log('here');
+ // text ='start'
+ //alert("myFunct"+text);
+// alert("Out"+text);
+  $.getJSON(myUrl+"?text="+text, function (jsonData) {
+            console.log(jsonData, "test");
+
+ // $(function(){ // on dom ready
+
+        $('#cy').cytoscape({
+          layout: {
+            name: 'cose',
+            padding: 10
+          },
+          
+          style: cytoscape.stylesheet()
+            .selector('node')
+              .css({
+                'shape': 'data(faveShape)',
+                'width': 'mapData(weight, 40, 80, 20, 60)',
+                'content': 'data(name)',
+                'text-valign': 'center',
+                'text-outline-width': 2,
+                'text-outline-color': 'data(faveColor)',
+                'background-color': 'data(faveColor)',
+                'color': '#fff'
+              })
+            .selector(':selected')
+              .css({
+                'border-width': 3,
+                'border-color': '#333'
+              })
+            .selector('edge')
+              .css({
+                'opacity': 0.666,
+                'width': 'mapData(strength, 70, 100, 2, 6)',
+                'target-arrow-shape': 'triangle',
+                'source-arrow-shape': 'circle',
+                'line-color': 'data(faveColor)',
+                'source-arrow-color': 'data(faveColor)',
+                'target-arrow-color': 'data(faveColor)'
+              })
+            .selector('edge.questionable')
+              .css({
+                'line-style': 'dotted',
+                'target-arrow-shape': 'diamond'
+              })
+            .selector('.faded')
+              .css({
+                'opacity': 0.25,
+                'text-opacity': 0
+              }),
+
+              elements: jsonData,
+          
+              ready: function(){
+                window.cy = this;
+                //console.log("Hello dude");
+                // giddy up
+              }
+         
+        });
+ // }); // on dom ready
+});
+
+}
+//}
+//}
